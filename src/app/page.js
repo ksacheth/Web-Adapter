7a0ff994b7e7474de746dce1fa053da4b41d036c
Sacheth Koushal <punnasachethkoushal@gmail.com>
2025-09-21 18:58:52 +0530

    added mainet and devnet

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 "use client";
-
+import { useState } from "react";
 import {
   ConnectionProvider,
   WalletProvider,
@@ -16,14 +16,24 @@ import Airdrop from "./components/Airdrop";
 import SignMessage from "./components/SignMessage";
 import SendSol from "./components/SendSol";
 export default function Home() {
+  const[network,setNetwork]=useState("https://api.devnet.solana.com");
+  const setMain = () => {
+    setNetwork("https://api.mainnet-beta.solana.com");
+  }
+  const setDev = () => {
+    setNetwork("https://api.devnet.solana.com");
+  }
   return (
-    <ConnectionProvider endpoint="https://api.devnet.solana.com">
+    <ConnectionProvider endpoint={network}>
       <WalletProvider wallets={[]} autoConnect>
         <WalletModalProvider>
           <div className="min-h-screen flex items-center justify-center">
             <div className="w-[60vw] h-[60vh] bg-blue-300 rounded-4xl">
               <div className="my-10 mx-10 flex justify-between">
+                
                 <WalletMultiButton />
+                <button onClick={setMain} className="bg-[#512DA7] px-10 py-2 text-white rounded-2xl font-semibold cursor-pointer hover:bg-black">Mainet</button>
+                <button onClick={setDev} className="bg-[#512DA7] px-10 py-2 text-white rounded-2xl font-semibold cursor-pointer hover:bg-black">Devnet</button>
                 <WalletDisconnectButton />
               </div>
               <div className="flex justify-center mb-10">
