d17d64c0d75297517da876dac9885a641193e891
Sacheth Koushal <punnasachethkoushal@gmail.com>
2025-09-22 00:05:01 +0530

    Changes in buttons

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { act, useState } from "react";
 import {
   ConnectionProvider,
   WalletProvider,
@@ -16,13 +16,18 @@ import Airdrop from "./components/Airdrop";
 import SignMessage from "./components/SignMessage";
 import SendSol from "./components/SendSol";
 export default function Home() {
-  const[network,setNetwork]=useState("https://api.devnet.solana.com");
+  const [network, setNetwork] = useState("https://api.devnet.solana.com");
+  const [activeButton, setActiveButton] = useState(0);
   const setMain = () => {
     setNetwork("https://api.mainnet-beta.solana.com");
-  }
+    alert("Switched to Mainnet");
+    setActiveButton(0);
+  };
   const setDev = () => {
     setNetwork("https://api.devnet.solana.com");
-  }
+    alert("Switched to Devnet");
+    setActiveButton(1);
+  };
   return (
     <ConnectionProvider endpoint={network}>
       <WalletProvider wallets={[]} autoConnect>
@@ -30,10 +35,21 @@ export default function Home() {
           <div className="min-h-screen flex items-center justify-center">
             <div className="w-[60vw] h-[60vh] bg-blue-300 rounded-4xl">
               <div className="my-10 mx-10 flex justify-between">
-                
                 <WalletMultiButton />
-                <button onClick={setMain} className="bg-[#512DA7] px-10 py-2 text-white rounded-2xl font-semibold cursor-pointer hover:bg-black">Mainet</button>
-                <button onClick={setDev} className="bg-[#512DA7] px-10 py-2 text-white rounded-2xl font-semibold cursor-pointer hover:bg-black">Devnet</button>
+                <button
+                  onClick={setMain}
+                  className="bg-[#512DA7] px-10 py-2 text-white rounded-2xl font-semibold cursor-pointer hover:bg-black"
+                  style={{ backgroundColor: activeButton === 0 ? "black" : "#512DA7" }}
+                >
+                  Mainet
+                </button>
+                <button
+                  onClick={setDev}
+                  className="bg-[#512DA7] px-10 py-2 text-white rounded-2xl font-semibold cursor-pointer hover:bg-black"
+                  style={{ backgroundColor: activeButton === 1 ? "black" : "#512DA7" }}
+                >
+                  Devnet
+                </button>
                 <WalletDisconnectButton />
               </div>
               <div className="flex justify-center mb-10">
